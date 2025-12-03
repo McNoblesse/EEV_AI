927c27ad6e99665a9d2252d0f06c0c7041ca0d2b
AlgoRealmOfficial <98455758+AlgoRealmOfficial@users.noreply.github.com>
2025-12-03 15:12:21 +0100

    Revert "Update on main file"
    This reverts commit a1ac2dec0d2ff98bdf23fa889cba5d05ab75490c.

--- app/main.py
+++ app/main.py
@@ -16,6 +16,3 @@ app.include_router(conversation_end_check.router)
 @app.get("/")
 async def root():
     return {"message": "Welcome to the eeV-AI Bot API"}
-
-## To run the app, use the command:
-## uvicorn app.main:app --reload
